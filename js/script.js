1fcc24e1a893077604c75eb071eb672f13ea22b5
nizar clima <casluu@outlook.es>
2022-06-15 17:07:05 -0300

    cambios finales

--- js/script.js
+++ js/script.js
@@ -75,6 +75,10 @@ function showWeather(data) {
     const min = kelvinToCentigrade(temp_min);
     const max = kelvinToCentigrade(temp_max);
 
+/*GUARDADO EN LOCALSTORAGE*/
+    localStorage.setItem("name", JSON.stringify(name))
+    localStorage.setItem("degrees", JSON.stringify(degrees))
+
     const content = document.createElement('div');
     content.innerHTML = `
         <h5>Clima en ${name}</h5>
@@ -84,9 +88,8 @@ function showWeather(data) {
         <p>Min: ${min}°C</p>
     `;
 
+
     result.appendChild(content);
-
-
 }
 
 /*MENSAJE DE ERROR*/
@@ -115,6 +118,27 @@ function clearHTML() {
 
 
 
+/*MODO CLARO CON LOCALSTORAGE*/
 
+const btnSwitch = document.querySelector('#switch');
 
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('light');
+	btnSwitch.classList.toggle('active');
 
+// Guardamos el modo en localstorage.
+	if(document.body.classList.contains('light')){
+		localStorage.setItem('light-mode', 'true');
+	} else {
+		localStorage.setItem('light-mode', 'false');
+	}
+});
+
+// Obtenemos el modo actual.
+if(localStorage.getItem('light-mode') === 'true'){
+	document.body.classList.add('light');
+	btnSwitch.classList.add('active');
+} else {
+	document.body.classList.remove('light');
+	btnSwitch.classList.remove('active');
+}
