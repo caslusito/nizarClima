366467e7a0a5abbb8ce32fbc35fc65a434d4f31c
nizar clima <casluu@outlook.es>
2022-06-16 21:07:44 -0300

    cambios finales

--- js/script.js
+++ js/script.js
@@ -118,22 +118,22 @@ function clearHTML() {
 const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () => {
-	document.body.classList.toggle('light');
-	btnSwitch.classList.toggle('active');
+    document.body.classList.toggle('light');
+    btnSwitch.classList.toggle('active');
 
-// Guardamos el modo en localstorage.
-	if(document.body.classList.contains('light')){
-		localStorage.setItem('light-mode', 'true');
-	} else {
-		localStorage.setItem('light-mode', 'false');
-	}
+    // Guardamos el modo en localstorage.
+    if (document.body.classList.contains('light')) {
+        localStorage.setItem('light-mode', 'true');
+    } else {
+        localStorage.setItem('light-mode', 'false');
+    }
 });
 
 // Obtenemos el modo actual.
-if(localStorage.getItem('light-mode') === 'true'){
-	document.body.classList.add('light');
-	btnSwitch.classList.add('active');
+if (localStorage.getItem('light-mode') === 'true') {
+    document.body.classList.add('light');
+    btnSwitch.classList.add('active');
 } else {
-	document.body.classList.remove('light');
-	btnSwitch.classList.remove('active');
+    document.body.classList.remove('light');
+    btnSwitch.classList.remove('active');
 }
